bc1f2a3a8b71a64441a4c597d977dcb62f500bdf
Austin Green <austin.green@delphix.com>
2016-10-12 14:26:52 -0700

    Initial repo push

---
+++ README.md
@@ -0,0 +1,50 @@
+karma-virtualboxany-launcher
+===================
+
+Launcher for using any browser in a Virtualbox VM.
+
+This is a simple implementation for working primarily with multiple browser types in Windows VMs.
+
+There are definitely bugs with this so please remember to report anything you find in the [issues](https://github.com/austin94/karma-virtualboxany-launcher/issues).
+
+## Installation
+Install Virtualbox 5.X+ here [virtualbox.org](https://www.virtualbox.org/).
+
+For this example I will be using the launcher to test with IE10 from a free VM provided by Microsoft.  [Download](https://developer.microsoft.com/en-us/microsoft-edge/tools/vms/) and import the OVA file into Virtualbox.
+
+Install the package as a development dependency.
+```
+npm install karma-virtualboxany-launcher --save-dev
+```
+
+## Configure your karma.conf.js
+```
+...
+customLaunchers: {
+    'virtualIE10': {
+        base: 'VirtualBoxAny',
+        config: {
+            vm_name: 'IE10 - Win7', // required - name of the VM to target
+            use_gui: false, // optional - allows VM to start headless
+            cmd: 'C:\\Program Files\\Internet Explorer\\iexplore.exe' // required - can be a path to any browser
+        }
+    },
+    // This isn't limited to IE only. Manually open a VM and install any browser you would like to test.
+    'virtualFirefox': {
+        base: 'VirtualBoxAny',
+        config: {
+            vm_name: 'IE10 - Win7', // required - name of the VM to target
+            use_gui: false, // optional - allows VM to start headless
+            cmd: 'C:\\Program Files\\Mozilla Firefox\\firefox.exe' // required - can be a path to any browser
+        }
+    },
+},
+
+// start these browsers
+// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+browsers: ['virtualIE10', 'virtualFirefox'],
+...
+```
+## Limitations / Known Issues
+* This may not work with VM's that are not Windows based
+* After Karma is stopped the VM's are left open.  This is on purpose to allow tests to be re-ran quickly.
---
+++ index.js
@@ -0,0 +1,93 @@
+var url = require('url');
+var virtualbox = require('virtualbox');
+var _ = require('lodash');
+
+function VirtualBoxAnyInstance(baseBrowserDecorator, logger, args) {
+    baseBrowserDecorator(this);
+    var log = logger.create('VirtualBoxAny: ');
+
+    args.config = _.defaults(args.config, {
+        vm_name: null,
+        user: 'IEUser',
+        password: 'Passw0rd!',
+        use_gui: false,
+        shutdown : false,
+        cmd: 'C:\\Program Files\\Internet Explorer\\iexplore.exe'
+    });
+
+    if (!args.config.vm_name) {
+        throw new Error('No VirtualBox VM name was provided in config.');
+    }
+
+    this.credentials = {
+        user: args.config.user,
+        password: args.config.password
+    };
+
+    this._getOptions = function(url) {
+        var urlObj = url.parse(url, true);
+        handleXUaCompatible(args, urlObj);
+        delete urlObj.search; //url.format does not want search attribute
+        url = url.format(urlObj);
+
+        return [
+            '-extoff'
+        ].concat(args.flags || {}, [url]);
+
+        function handleXUaCompatible(args, urlObj) {
+            if (args['x-ua-compatible']) {
+                urlObj.query['x-ua-compatible'] = args['x-ua-compatible'];
+            }
+        }
+    };
+
+    this._start = function(url) {
+        var self = this;
+        var conf = {
+          user: self.credentials.user,
+          password: self.credentials.password,
+          vm: args.config.vm_name,
+          cmd: args.config.cmd,
+          params: url
+        };
+
+        conf.params = conf.params.replace('localhost', '10.0.2.2');
+
+        log.debug('Starting virtualbox vm [' + conf.vm + '] with url ' + conf.params);
+
+        virtualbox.start(conf.vm, args.config.use_gui, function(err) {
+            if (err) { throw err; }
+            self.openProcess(conf);
+        });
+    };
+
+    this.openProcess = function(conf) {
+      var self = this;
+      log.debug('Attempting to open command: ' + conf.cmd);
+
+      virtualbox.exec(conf, function(err){
+        if (err && /The guest execution service is not ready/.test(err.message)) {
+          log.debug('Host VM not loaded. Waiting...');
+
+          setTimeout(function() {
+            self.openProcess(conf);
+          }, 10000);
+        } else if (err) {
+          log.debug(err);
+        }
+      });
+    };
+}
+
+VirtualBoxAnyInstance.$inject = ['baseBrowserDecorator', 'logger', 'args'];
+VirtualBoxAnyInstance.prototype = {
+    name: 'VirtualBoxAny',
+    DEFAULT_CMD: {
+        win32: 'C:\\Program Files\\Internet Explorer\\iexplore.exe'
+    },
+    ENV_CMD: 'IE_BIN'
+};
+
+module.exports = {
+    'launcher:VirtualBoxAny': ['type', VirtualBoxAnyInstance]
+};
